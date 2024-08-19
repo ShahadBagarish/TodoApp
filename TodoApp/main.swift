0f2874cd63d893e6f37265086bf377ddd4233826
Shahad Bagarish <Shahad.bagarish@gmail.com>
2024-08-19 09:55:57 +0300

    InMemoryCache finished

--- TodoApp/main.swift
+++ TodoApp/main.swift
@@ -15,34 +15,26 @@ struct Todo: CustomStringConvertible , Codable {
     var isCompleted: Bool = false
 }
 
-// Create the `Cache` protocol that defines the following method signatures:
-//  `func save(todos: [Todo])`: Persists the given todos.
-//  `func load() -> [Todo]?`: Retrieves and returns the saved todos, or nil if none exist.
 protocol Cache {
     func save(todos: [Todo])
     func load() -> [Todo]?
 }
 
-// `FileSystemCache`: This implementation should utilize the file system
-// to persist and retrieve the list of todos.
-// Utilize Swift's `FileManager` to handle file operations.
-
-
 class FileSystemCache: Cache {
     
     let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("userProfile.json")
     var readingData: [Todo] = []
-
+    
     
     func save(todos: [Todo]) {
         readingData = todos
         
         do {
-         let jsonEncoder = JSONEncoder()
-         let jsonData = try jsonEncoder.encode(readingData)
+            let jsonEncoder = JSONEncoder()
+            let jsonData = try jsonEncoder.encode(readingData)
             try jsonData.write(to: self.path)
         } catch {
-         print("Error encoding data: \(error)")
+            print("Error encoding data: \(error)")
         }
     }
     
@@ -52,7 +44,7 @@ class FileSystemCache: Cache {
             let jsonData = try Data(contentsOf: self.path)
             
             let jsonDecoder = JSONDecoder()
-            let readingData = try jsonDecoder.decode([Todo].self, from: jsonData)
+            _ = try jsonDecoder.decode([Todo].self, from: jsonData)
             
         } catch {
             print("Error decoding data: \(error)")
@@ -66,25 +58,27 @@ class FileSystemCache: Cache {
 // `InMemoryCache`: : Keeps todos in an array or similar structure during the session.
 // This won't retain todos across different app launches,
 // but serves as a quick in-session cache.
-//final class InMemoryCache: Cache {
-//    func save(todos: [Todo]) {
-//        
-//    }
-//    
-//    func load() -> [Todo]? {
-//        
-//    }
-//    
-//    
-//}
+class InMemoryCache: Cache {
+    
+    var savedData: [Todo]?
+    
+    func save(todos: [Todo]) {
+        savedData = todos
+    }
+    
+    func load() -> [Todo]? {
+        return savedData
+    }
+    
+    
+}
 
-final class TodoManager: FileSystemCache {
+final class TodoManager: InMemoryCache {
     var tasks: [Todo] = []
     
     func listTodos(){
         print("\n 📝 Your Todos: ")
-        var loadData = load()
-        if let load = loadData{
+        if let load = load() {
             for index in load.indices {
                 print("    \(index+1). \(load[index].isCompleted ? "✅" : "❌") \(load[index].title) ")
             }
@@ -97,17 +91,24 @@ final class TodoManager: FileSystemCache {
         tasks.append(task)
         save(todos: tasks)
     }
-
-    func toggleCompletion(at index: Int, all: inout [Todo]){
-        if index > all.count{
-            print("\n Number out of scope \n")
-        }else{
-            print("\n 🔄 Todo completion status is toggled! \n")
-            return all[index-1].isCompleted = true
+    
+    func toggleCompletion(at index: Int){
+        if let loadData = load(){
+            if index > loadData.count{
+                print("\n Number out of scope \n")
+            }else{
+                print("\n 🔄 Todo completion status is toggled! \n")
+                tasks[index-1].isCompleted = true
+                save(todos: tasks)
+            }
         }
+        
     }
-    func delete(at index: Int, all: inout [Todo]){
-        all.remove(at: index-1)
+    func delete(at index: Int){
+        if let load = load() {
+            tasks.remove(at: index-1)
+            save(todos: tasks)
+        }
         print("\n 🗑️ Todo Deleted \n")
     }
 }
@@ -122,10 +123,10 @@ final class App {
         case delete
     }
     
-
+    
     func run(){
+
         let manager = TodoManager()
-        var tasks: [Todo] = []
         var breaker = ""
         
         repeat {
@@ -147,9 +148,9 @@ final class App {
                     case .delete:
                         manager.listTodos()
                         print("\n Enter the number of the todo to delete: \n")
-                        if var number = readLine() {
-                            if var num = Int(number) {
-                                manager.delete(at: Int(num), all: &tasks)
+                        if let number = readLine() {
+                            if let num = Int(number) {
+                                manager.delete(at: Int(num))
                             }
                         }
                         
@@ -158,9 +159,9 @@ final class App {
                         
                     case .toggle:
                         print("\n Enter the number of todo to toggle: ")
-                        if var number = readLine() {
-                            if var num = Int(number) {
-                                manager.toggleCompletion(at: Int(num), all: &tasks)
+                        if let number = readLine() {
+                            if let num = Int(number) {
+                                manager.toggleCompletion(at: Int(num))
                             }
                         }
                     }
